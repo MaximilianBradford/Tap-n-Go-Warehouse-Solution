bcfde71815e2348bfef3b95276f400c20aaab268
MaximilianBradford <maximilian.bradford@edu.sait.ca>
2025-04-20 16:25:07 -0600

    Locked Orientation to Portrait
    Lock orientation to portrait to prevent issues from the screen flipping

--- App/app/src/main/java/dev/tapngo/app/MainActivity.kt
+++ App/app/src/main/java/dev/tapngo/app/MainActivity.kt
@@ -373,6 +373,6 @@ var authToken: String? = null
 
 // Constants for my testing servers ~ Dan
 //const val server = "10.0.2.2:8000" // Localhost
-const val server = "10.0.0.116:8080" // Desktop
+const val server = "10.0.0.116" // Desktop
 
 //const val server = "###.###.###.###:8080" // Garage servers. (not posting the IP here)
